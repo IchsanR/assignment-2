87920d8a1dc01a467480179994f9dbaec4ac5175
Ichsan Ramadhan <ichsan.ramadhan32@gmail.com>
2024-03-09 18:00:22 +0700

    feat: init env config

---
+++ config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"github.com/caarlos0/env"
+	"github.com/joho/godotenv"
+)
+
+type EnvConfig struct {
+	DbHost     string `env:"DB_HOSTNAME" env-default:"localhost"`
+	DbPort     int    `env:"DB_PORT" env-default:"5432"`
+	DbUser     string `env:"DB_USERNAME" env-default:"postgres"`
+	DbPassword string `env:"DB_PASSWORD" env-default:"root"`
+	DbName     string `env:"DB_NAME" env-defauld:"assignment2"`
+}
+
+func LoadEnv() EnvConfig {
+	cfg := EnvConfig{}
+
+	err := godotenv.Load()
+	if err != nil {
+		panic("cannot load env file")
+	}
+
+	err = env.Parse(&cfg)
+	if err != nil {
+		panic("cannot load env file")
+	}
+	return cfg
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module assigntment2
 
 go 1.21.4
+
+require (
+	github.com/caarlos0/env v3.5.0+incompatible
+	github.com/joho/godotenv v1.5.1
+)
