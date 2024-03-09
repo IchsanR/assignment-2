dbadb3783d4c8cc5879f9cd176d1c259efaa4a91
Ichsan Ramadhan <ichsan.ramadhan32@gmail.com>
2024-03-09 18:09:21 +0700

    feat: initialization routers

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "assigntment2/database"
+import (
+	"assigntment2/database"
+	"assigntment2/routers"
+)
 
 func main() {
+	var PORT = ":8080"
 	database.DatabaseConnect()
+
+	routers.StartServer().Run(PORT)
 }
