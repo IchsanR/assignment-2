b849febce263bc1a0dbb8487bb1e11138059d578
Ichsan Ramadhan <ichsan.ramadhan32@gmail.com>
2024-03-13 14:26:37 +0700

    feat: add update product endpoint

--- routers/router.go
+++ routers/router.go
@@ -16,6 +16,7 @@ func StartServer(db *sql.DB) *gin.Engine {
 
 	router.POST("/orders", orderController.CreateOrder)
 	router.GET("/orders", orderController.GetAllOrders)
+	router.PUT("/orders/:id", orderController.UpdateOrder)
 	router.DELETE("/orders/:id", orderController.DeleteOrder)
 
 	return router
